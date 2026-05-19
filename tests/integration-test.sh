df0f3d8bde01f9ea542268d14d0dacae20d012dd
Satoru Sato <satoru.satoh@gmail.com>
2026-05-19 11:46:45 +0900

    fix: enable tls (do NOT disable with --tls-verify=false)

--- tests/integration-test.sh
+++ tests/integration-test.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 set -ex -o pipefail
 
+#opt="--tls-verify=false"
+opt=""
+
+#registry=localhost
+registry=registry.lab-10.local
 
 systemctl is-active zot || sudo systemctl start zot
 
 # push
 podman pull alpine:latest
-podman tag alpine:latest localhost:5000/test-alpine-0:latest
-podman push localhost:5000/test-alpine-0:latest  --tls-verify=false
+podman tag alpine:latest ${registry}:5000/test-alpine-0:latest
+podman push ${registry}:5000/test-alpine-0:latest ${opt}
 
 # pull
-podman rmi localhost:5000/test-alpine-0:latest
-podman pull localhost:5000/test-alpine-0:latest  --tls-verify=false
+podman rmi ${registry}:5000/test-alpine-0:latest
+podman pull ${registry}:5000/test-alpine-0:latest ${opt}
 
 # cleanup
-podman rmi localhost:5000/test-alpine-0:latest
+podman rmi ${registry}:5000/test-alpine-0:latest
